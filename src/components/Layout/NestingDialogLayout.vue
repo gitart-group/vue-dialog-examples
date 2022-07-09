902bcbdac2da91a9e8ec2eda622d695ac64de94e
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2022-07-09 03:12:23 +0300

    feat: upgrade to 2.4.0. add example for the close-on-back

--- src/components/Layout/NestingDialogLayout.vue
+++ src/components/Layout/NestingDialogLayout.vue
@@ -4,67 +4,82 @@
       Nested Dialogs
     </h3>
 
-    <GDialog
-      v-model="dialogs.first"
-      max-width="400"
-    >
-      <DialogToolbar @close="dialogs.first = false" />
+    <label class="flex mb-2">
+      <input
+        v-model="closeOnBack"
+        type="checkbox"
+      >
+      <span class="font-semibold ml-2">
+        Close on Back
+      </span>
+    </label>
 
-      <div p="x-5 y-4">
-        <h3 m="b-4">
-          First Dialog
-        </h3>
+    <div :key="closeOnBack ? '1' : '2'">
+      <GDialog
+        v-model="dialogs.first"
+        max-width="400"
+        :close-on-back="closeOnBack"
+      >
+        <DialogToolbar @close="dialogs.first = false" />
 
-        <p m="b-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quas aliquid, voluptates voluptas commodi inventore.
-        </p>
+        <div p="x-5 y-4">
+          <h3 m="b-4">
+            First Dialog
+          </h3>
 
-        <Btn m="r-2" @click="dialogs.second = true">
-          Second
-        </Btn>
+          <p m="b-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quas aliquid, voluptates voluptas commodi inventore.
+          </p>
 
-        <Btn m="r-2" @click="dialogs.third = true">
-          Third
-        </Btn>
-      </div>
-    </GDialog>
+          <Btn m="r-2" @click="dialogs.second = true">
+            Second
+          </Btn>
 
-    <GDialog
-      v-model="dialogs.second"
-      fullscreen
-    >
-      <DialogToolbar @close="dialogs.second = false" />
+          <Btn m="r-2" @click="dialogs.third = true">
+            Third
+          </Btn>
+        </div>
+      </GDialog>
 
-      <div p="x-5 y-4">
-        <h3 m="b-4">
-          Second Dialog
-        </h3>
+      <GDialog
+        v-model="dialogs.second"
+        fullscreen
+        :close-on-back="closeOnBack"
+      >
+        <DialogToolbar @close="dialogs.second = false" />
 
-        <p m="b-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, sit.
-        </p>
+        <div p="x-5 y-4">
+          <h3 m="b-4">
+            Second Dialog
+          </h3>
 
-        <Btn m="r-2" @click="dialogs.third = true">
-          Third
-        </Btn>
-      </div>
-    </GDialog>
+          <p m="b-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, sit.
+          </p>
 
-    <GDialog
-      v-model="dialogs.third"
-      max-width="500"
-    >
-      <DialogToolbar @close="dialogs.third = false" />
+          <Btn m="r-2" @click="dialogs.third = true">
+            Third
+          </Btn>
+        </div>
+      </GDialog>
 
-      <div p="x-5 y-4">
-        <h3 m="b-4">
-          Third Dialog
-        </h3>
-        <p m="b-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio iusto aperiam quasi recusandae nulla officia quibusdam libero et alias minus?
-        </p>
-      </div>
-    </GDialog>
+      <GDialog
+        v-model="dialogs.third"
+        max-width="500"
+        :close-on-back="closeOnBack"
+      >
+        <DialogToolbar @close="dialogs.third = false" />
+
+        <div p="x-5 y-4">
+          <h3 m="b-4">
+            Third Dialog
+          </h3>
+          <p m="b-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio iusto aperiam quasi recusandae nulla officia quibusdam libero et alias minus?
+          </p>
+        </div>
+      </GDialog>
+    </div>
 
     <Btn
       m="r-2"
@@ -102,6 +117,8 @@ export default defineComponent({
   },
 
   setup() {
+    const closeOnBack = ref(false)
+
     const dialogs = reactive({
       first: false,
       second: false,
@@ -109,6 +126,7 @@ export default defineComponent({
     })
 
     return {
+      closeOnBack,
       dialogs,
     }
   },
